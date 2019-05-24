7a788409c17fa67fd468489a8369aa39503726a0
Battistini <battistini.anthony@gmail.com>
2019-05-24 18:31:05 +0200

    TP03

---
+++ TP01-Premier_projet_maven/src/main/java/dev/App.java
@@ -0,0 +1,11 @@
+package dev;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( FigletFont.convertOneLine("App") );
+    }
+}
